18fb796478d40ff07d1d44fbc05fa5b6593a800e
Pedro Henrique <venanciosampaioph@gmail.com>
2026-09-18 17:52:58 -0300

    Adição do try e except

--- MyLife.py
+++ MyLife.py
@@ -7,12 +7,17 @@ print("            MyLife")
 print("===============================")
 
 while True:
+
     print("\n1-Adicionar Tarefa") #Interface do usuário
     print("2-Listar Tarefas")  #Interface do usuário
     print("3-Concluir Tarefa")  #Interface do usuário
     print("4-Excluir Tarefa")  #Interface do usuário
     print("5-Sair")  #Interface do usuário
-    opcao = int(input("Escolha uma opção: "))
+    try:
+
+        opcao = int(input("Escolha uma opção: "))
+    except ValueError:
+        print("Digite apenas numeros!!")
     if opcao == 5: 
         print("Encerrando o MyLife...")
         break
@@ -34,19 +39,28 @@ while True:
             print("Nenhuma tarefa cadastrada.")
 
         else:
-            numero = int(input("Digite o numero da tarefa que deseja concluir: "))
+         try:
+            
+             numero = int(input("Digite o numero da tarefa que deseja concluir: "))
+         except ValueError:
+                print("Digite apenas numeros!!")
+                continue   
 
-            tarefa_concluida = tarefas.pop(numero - 1)
+         tarefa_concluida = tarefas.pop(numero - 1)
 
-            print("Tarefa concluída com sucesso:", tarefa_concluida)
+         print("Tarefa concluída com sucesso:", tarefa_concluida)
 
     if opcao == 4:
         if not tarefas:
             print("nenhuma tarefa cadastrada.")
         else:
+         try:
             numero = int(input("Digite o numero da tarefa que deseja remover:"))
-            tarefa_removida = tarefas.pop(numero - 1)
-            print("Tarefa Removida.")
+         except ValueError:
+                    print("Digite apenas numeros!!")
+                    continue
+         tarefa_removida = tarefas.pop(numero - 1)
+        print("Tarefa Removida.")
 
               
 
